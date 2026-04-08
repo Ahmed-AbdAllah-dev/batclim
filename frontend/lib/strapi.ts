8f98c6b666290645d639905add00b3d88e659dcf
Ahmed-AbdAllah-dev <hmedbana12@gmail.com>
2026-04-08 08:58:09 +0100

    feat: add backend wake-up alert and extended timeout

--- frontend/lib/strapi.ts
+++ frontend/lib/strapi.ts
@@ -2,20 +2,27 @@
 const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL ;
 
 console.log('Strapi API URL:', API_URL); // This will help debug
-
+var mutex = 0;
 // Helper to fetch data from Strapi
 export async function fetchStrapi<T>(path: string): Promise<T> {
   const url = `${API_URL}/api${path}`;
   console.log('Fetching from:', url); // This will help debug
-  
+  const hasSeenWarning = localStorage.getItem('hasSeenRenderWarning');
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), 90000);
+  if(!hasSeenWarning){
+  alert("The server is waking up! This may take up to 90 seconds.");
+  localStorage.setItem('hasSeenRenderWarning','true');
+  }
   const response = await fetch(url, {
-    next: { revalidate: 60 }, // ISR: revalidate every 60 seconds
+    next: { revalidate: 60 },// ISR: revalidate every 60 seconds
+    signal: controller.signal, 
   });
   
   if (!response.ok) {
     throw new Error(`Failed to fetch from Strapi: ${response.statusText}`);
   }
-  
+  clearTimeout(id);
   const data = await response.json();
   console.log('Strapi response:', data); // This will help debug
   return data.data;
